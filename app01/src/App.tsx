ea8e051894a334ea8ed81a0f79c2f382ee41b7d1
Pasha <pashaie@gmail.com>
2023-05-23 21:46:35 +0330

    1

--- app01/src/App.tsx
+++ app01/src/App.tsx
@@ -3,6 +3,12 @@ import React, { useState } from "react";
 import { Logo } from "./components/Logo";
 import { Counter } from "./components/Counter";
 
+const tasks = [
+  { name: "task 1", id: 1, done: true },
+  { name: "task 2", id: 2, done: false },
+  { name: "task 3", id: 3, done: true },
+  { name: "task 4", id: 4, done: true },
+];
 //Component React -> PascalCase
 function App() {
   const [showLogo, setShowLogo] = useState(true);
@@ -14,6 +20,9 @@ function App() {
       <button onClick={toggle}>toggle</button>
       {showLogo && <Logo msg="salam" icon="⚛️" />}
       <Counter />
+      {tasks.map((m) => (
+        <>{m.name}</>
+      ))}
     </>
   );
 }
