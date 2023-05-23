c7ba2d4cfbd28c62b05820459e600eb18e1207be
Pasha <pashaie@gmail.com>
2023-05-23 20:35:14 +0330

    app01/src/App:

--- app01/src/App.tsx
+++ app01/src/App.tsx
@@ -6,10 +6,11 @@ import React, { useState } from "react";
 function App() {
   //State
   //Hook
-
-  const stateArray = useState(10);
-  const counter = stateArray[0];
-  const setCounter = stateArray[1];
+  // const stateArray = useState(10);
+  // const counter = stateArray[0];
+  // const setCounter = stateArray[1];
+  //Destructure
+  const [counter, setCounter] = useState(10);
 
   const inc = () => {
     setCounter(counter + 1);
@@ -22,11 +23,14 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+      {counter !== 10 ? (
+        <div>
+          <a href="https://react.dev" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
+        </div>
+      ) : null}
+
       <h1>Counter: {counter} </h1>
       <button onClick={inc}>➕</button>
       <button onClick={dec}>➖</button>
