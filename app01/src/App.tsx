90a4a113a1184ca002ea3c625b2704edfa4bb033
Pasha <pashaie@gmail.com>
2023-05-23 19:39:40 +0330

    app01/src/App:  function syntax

--- app01/src/App.tsx
+++ app01/src/App.tsx
@@ -2,7 +2,21 @@ import reactLogo from "./assets/react.svg";
 import "./App.css";
 import React from "react";
 
+//win 10, win 11  -> win + .
 function App() {
+  let counter = 0;
+  const inc = () => {
+    counter += 1;
+  };
+  //Expression
+  const incFn1 = function () {
+    counter += 1;
+  };
+  //Statement
+  function incFn2() {
+    //this
+    counter += 1;
+  }
   return (
     <>
       <div>
@@ -10,6 +24,9 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
+      <h1>Counter: {counter} </h1>
+      <button onClick={inc}>➕</button>
+      <button>➖</button>
     </>
   );
 }
